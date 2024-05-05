d5f5b6c2173f1c6c1be79d94b7d41bcc7669f851
kaif_sayyed <sayyedkaifoppo@gmail.com>
2024-05-05 04:40:02 -0700

    create middleware of route

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,6 @@
 <div class='alert alert-success'>{{session('success')}}</div>
 @endif
 
-<p>hello</p>
-
-
 
 <div class="row justify-content-center mt-5">
     <div class="col-md-8">
--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('register',[RegisterController::class,'register'])->name('register');
-Route::post('/store',[RegisterController::class,'store'])->name('store');
-Route::get('/login',[RegisterController::class,'login'])->name('login');
-Route::post('/authenticate',[RegisterController::class,'authenticate'])->name('authenticate');
+Route::get('register', [RegisterController::class, 'register'])->name('register');
+Route::post('/store', [RegisterController::class, 'store'])->name('store');
+Route::get('/login', [RegisterController::class, 'login'])->name('login');
+Route::post('/authenticate', [RegisterController::class, 'authenticate'])->name('authenticate');
 
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', [RegisterController::class, 'home'])->name('home');
+    Route::get('/logout', [RegisterController::class, 'logout'])->name('logout');
+});
 
-Route::get('/home',[RegisterController::class,'home'])->name('home');
-Route::get('/logout',[RegisterController::class,'logout'])->name('logout');
